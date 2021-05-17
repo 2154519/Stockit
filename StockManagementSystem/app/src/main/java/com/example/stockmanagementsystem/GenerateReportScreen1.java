d207ddc4612aaa5add175eb3965258905c00a952
2154519 <69995579+2154519@users.noreply.github.com>
2021-05-17 08:46:10 +0200

    Views

---
+++ StockManagementSystem/app/src/main/java/com/example/stockmanagementsystem/GenerateReportScreen1.java
@@ -0,0 +1,14 @@
+package com.example.stockmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GenerateReportScreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_report_screen1);
+    }
+}
---
+++ StockManagementSystem/app/src/main/java/com/example/stockmanagementsystem/TakeInventoryScreen.java
@@ -0,0 +1,14 @@
+package com.example.stockmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TakeInventoryScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_inventory_screen);
+    }
+}
