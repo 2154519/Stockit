1343256f168bee17bdb79a8608fa4b050f3ecbc5
2154519 <69995579+2154519@users.noreply.github.com>
2021-05-20 13:42:11 +0200

    s

---
+++ StockManagementSystem/app/src/main/java/com/example/stockmanagementsystem/QRScanScreen.java
@@ -0,0 +1,14 @@
+package com.example.stockmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QRScanScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrscan_screen);
+    }
+}
--- StockManagementSystem/app/src/main/java/com/example/stockmanagementsystem/StartScreen.java
+++ StockManagementSystem/app/src/main/java/com/example/stockmanagementsystem/StartScreen.java
@@ -11,4 +11,6 @@ public class StartScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
     }
+
+
 }
